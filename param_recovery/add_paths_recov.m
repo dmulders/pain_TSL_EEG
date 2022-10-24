411c6f394ccecec941d172f2c75308f14cd85bbd
Dounia Mulders <dounia.mulders@gmail.com>
2022-10-24 16:50:53 -0400

    Update add_paths_recov.m

--- param_recovery/add_paths_recov.m
+++ param_recovery/add_paths_recov.m
@@ -1,12 +1,10 @@
 % Add paths to the different toolboxes at the beginning of a session
 
 
-% EEGLAB functions useful to do the scalp plots
+% useful functions to do the scalp plots
 addpath(genpath('../Colormaps')) ; 
 
-
 addpath(genpath('../MinimalTransitionProbsModel')) ; 
-
 
 % Low resolution on plot
 set(0, 'DefaultFigureRenderer', 'painters') ;
